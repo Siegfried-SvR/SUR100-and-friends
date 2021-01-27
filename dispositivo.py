86002badf90488a8a0bc5d4d137e242272696deb
Augusto <abonellitoro@gmail.com>
2021-01-27 13:35:53 -0300

    probando

--- dispositivo.py
+++ dispositivo.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
-
+#probando
 class Dispositivo:
     def __init__(self, name):
         self.name = name
